9def88a3e1375f844dd777e254722f8e334e1edf
H. Peter Anvin <hpa@linux.intel.com>
2013-04-27 16:10:11 -0700

    x86, flags: Rename X86_EFLAGS_BIT1 to X86_EFLAGS_FIXED
    Bit 1 in the x86 EFLAGS is always set.  Name the macro something that
    actually tries to explain what it is all about, rather than being a
    tautology.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Gleb Natapov <gleb@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Link: http://lkml.kernel.org/n/tip-f10rx5vjjm6tfnt8o1wseb3v@git.kernel.org

--- arch/x86/include/uapi/asm/processor-flags.h
+++ arch/x86/include/uapi/asm/processor-flags.h
@@ -7,44 +7,24 @@
 /*
  * EFLAGS bits
  */
-#define X86_EFLAGS_CF_BIT	0 /* Carry Flag */
-#define X86_EFLAGS_CF		_BITUL(X86_EFLAGS_CF_BIT)
-#define X86_EFLAGS_FIXED_BIT	1 /* Bit 1 - always on */
-#define X86_EFLAGS_FIXED	_BITUL(X86_EFLAGS_FIXED_BIT)
-#define X86_EFLAGS_PF_BIT	2 /* Parity Flag */
-#define X86_EFLAGS_PF		_BITUL(X86_EFLAGS_PF_BIT)
-#define X86_EFLAGS_AF_BIT	4 /* Auxiliary carry Flag */
-#define X86_EFLAGS_AF		_BITUL(X86_EFLAGS_AF_BIT)
-#define X86_EFLAGS_ZF_BIT	6 /* Zero Flag */
-#define X86_EFLAGS_ZF		_BITUL(X86_EFLAGS_ZF_BIT)
-#define X86_EFLAGS_SF_BIT	7 /* Sign Flag */
-#define X86_EFLAGS_SF		_BITUL(X86_EFLAGS_SF_BIT)
-#define X86_EFLAGS_TF_BIT	8 /* Trap Flag */
-#define X86_EFLAGS_TF		_BITUL(X86_EFLAGS_TF_BIT)
-#define X86_EFLAGS_IF_BIT	9 /* Interrupt Flag */
-#define X86_EFLAGS_IF		_BITUL(X86_EFLAGS_IF_BIT)
-#define X86_EFLAGS_DF_BIT	10 /* Direction Flag */
-#define X86_EFLAGS_DF		_BITUL(X86_EFLAGS_DF_BIT)
-#define X86_EFLAGS_OF_BIT	11 /* Overflow Flag */
-#define X86_EFLAGS_OF		_BITUL(X86_EFLAGS_OF_BIT)
-#define X86_EFLAGS_IOPL_BIT	12 /* I/O Privilege Level (2 bits) */
-#define X86_EFLAGS_IOPL		(_AC(3,UL) << X86_EFLAGS_IOPL_BIT)
-#define X86_EFLAGS_NT_BIT	14 /* Nested Task */
-#define X86_EFLAGS_NT		_BITUL(X86_EFLAGS_NT_BIT)
-#define X86_EFLAGS_RF_BIT	16 /* Resume Flag */
-#define X86_EFLAGS_RF		_BITUL(X86_EFLAGS_RF_BIT)
-#define X86_EFLAGS_VM_BIT	17 /* Virtual Mode */
-#define X86_EFLAGS_VM		_BITUL(X86_EFLAGS_VM_BIT)
-#define X86_EFLAGS_AC_BIT	18 /* Alignment Check/Access Control */
-#define X86_EFLAGS_AC		_BITUL(X86_EFLAGS_AC_BIT)
-#define X86_EFLAGS_AC_BIT	18 /* Alignment Check/Access Control */
-#define X86_EFLAGS_AC		_BITUL(X86_EFLAGS_AC_BIT)
-#define X86_EFLAGS_VIF_BIT	19 /* Virtual Interrupt Flag */
-#define X86_EFLAGS_VIF		_BITUL(X86_EFLAGS_VIF_BIT)
-#define X86_EFLAGS_VIP_BIT	20 /* Virtual Interrupt Pending */
-#define X86_EFLAGS_VIP		_BITUL(X86_EFLAGS_VIP_BIT)
-#define X86_EFLAGS_ID_BIT	21 /* CPUID detection */
-#define X86_EFLAGS_ID		_BITUL(X86_EFLAGS_ID_BIT)
+#define X86_EFLAGS_CF	0x00000001 /* Carry Flag */
+#define X86_EFLAGS_FIXED 0x00000002 /* Bit 1 - always on */
+#define X86_EFLAGS_PF	0x00000004 /* Parity Flag */
+#define X86_EFLAGS_AF	0x00000010 /* Auxiliary carry Flag */
+#define X86_EFLAGS_ZF	0x00000040 /* Zero Flag */
+#define X86_EFLAGS_SF	0x00000080 /* Sign Flag */
+#define X86_EFLAGS_TF	0x00000100 /* Trap Flag */
+#define X86_EFLAGS_IF	0x00000200 /* Interrupt Flag */
+#define X86_EFLAGS_DF	0x00000400 /* Direction Flag */
+#define X86_EFLAGS_OF	0x00000800 /* Overflow Flag */
+#define X86_EFLAGS_IOPL	0x00003000 /* IOPL mask */
+#define X86_EFLAGS_NT	0x00004000 /* Nested Task */
+#define X86_EFLAGS_RF	0x00010000 /* Resume Flag */
+#define X86_EFLAGS_VM	0x00020000 /* Virtual Mode */
+#define X86_EFLAGS_AC	0x00040000 /* Alignment Check */
+#define X86_EFLAGS_VIF	0x00080000 /* Virtual Interrupt Flag */
+#define X86_EFLAGS_VIP	0x00100000 /* Virtual Interrupt Pending */
+#define X86_EFLAGS_ID	0x00200000 /* CPUID detection flag */
 
 /*
  * Basic CPU control in CR0
