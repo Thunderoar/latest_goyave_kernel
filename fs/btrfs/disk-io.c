8d6285ec080cdf9a42d45120a64ddf24c576bd73
Stefan Behrens <sbehrens@giantdisaster.de>
2013-05-13 14:42:57 +0000

    Btrfs: cleanup, btrfs_read_fs_root_no_name() doesn't return NULL
    No need to check for NULL in send.c and disk-io.c.
    
    Signed-off-by: Stefan Behrens <sbehrens@giantdisaster.de>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -2823,8 +2823,6 @@ retry_root_backup:
 	location.offset = (u64)-1;
 
 	fs_info->fs_root = btrfs_read_fs_root_no_name(fs_info, &location);
-	if (!fs_info->fs_root)
-		goto fail_qgroup;
 	if (IS_ERR(fs_info->fs_root)) {
 		err = PTR_ERR(fs_info->fs_root);
 		goto fail_qgroup;
