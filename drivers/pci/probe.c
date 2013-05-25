67a5f9348959682c6ccc9194de56c7c4cec82e37
Gu Zheng <guz.fnst@cn.fujitsu.com>
2013-05-25 21:48:30 +0800

    PCI: Introduce pci_alloc_dev(struct pci_bus*) to replace alloc_pci_dev()
    Here we introduce a new interface to replace alloc_pci_dev():
    
        struct pci_dev *pci_alloc_dev(struct pci_bus *bus)
    
    It takes a "struct pci_bus *" argument, so we can alloc a PCI device
    on a target PCI bus, and it acquires a reference on the pci_bus.
    We use pci_alloc_dev(NULL) to simplify the old alloc_pci_dev(),
    and keep it for a while but mark it as __deprecated.
    
    Holding a reference to the pci_bus ensures that referencing
    pci_dev->bus is valid as long as the pci_dev is valid.
    
    [bhelgaas: keep existing "return error early" structure in pci_alloc_dev()]
    Signed-off-by: Gu Zheng <guz.fnst@cn.fujitsu.com>
    Signed-off-by: Jiang Liu <jiang.liu@huawei.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -1236,7 +1236,7 @@ static void pci_release_bus_bridge_dev(struct device *dev)
 	kfree(bridge);
 }
 
-struct pci_dev *alloc_pci_dev(void)
+struct pci_dev *pci_alloc_dev(struct pci_bus *bus)
 {
 	struct pci_dev *dev;
 
@@ -1246,8 +1246,15 @@ struct pci_dev *alloc_pci_dev(void)
 
 	INIT_LIST_HEAD(&dev->bus_list);
 	dev->dev.type = &pci_dev_type;
+	dev->bus = pci_bus_get(bus);
 
 	return dev;
+}
+EXPORT_SYMBOL(pci_alloc_dev);
+
+struct pci_dev *alloc_pci_dev(void)
+{
+	return pci_alloc_dev(NULL);
 }
 EXPORT_SYMBOL(alloc_pci_dev);
 
