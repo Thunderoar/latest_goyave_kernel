9a96401569859509ec9383407e97e7e7b551d47a
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-05-28 09:15:59 +0200

    usb: gadget: rndis: init & exit rndis at module load/unload
    This is required in preparation for using usb_gstrings_attach.
    
    The rndis initialization so far has been performed on the first
    occurence of rndis_bind(), but the condition to check it (first
    or not first) was "borrowed" from strings handling.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/rndis.c
+++ drivers/usb/gadget/rndis.c
@@ -1180,7 +1180,7 @@ int rndis_init(void)
 	rndis_initialized = true;
 	return 0;
 }
-EXPORT_SYMBOL(rndis_init);
+module_init(rndis_init);
 
 void rndis_exit(void)
 {
@@ -1200,6 +1200,6 @@ void rndis_exit(void)
 	}
 #endif
 }
-EXPORT_SYMBOL(rndis_exit);
+module_exit(rndis_exit);
 
 MODULE_LICENSE("GPL");
