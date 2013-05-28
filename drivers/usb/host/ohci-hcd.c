399e72b671a49798f1b147fe982e756e374dcde8
Manjunath Goudar <manjunath.goudar@linaro.org>
2013-05-28 18:34:51 +0530

    USB: OHCI: make ohci-pci a separate driver
    This patch splits the PCI portion of ohci-hcd out into its
    own separate driver module, called ohci-pci.
    
    The major point of difficulty lies in ohci-pci's many vendor- and
    device-specific workarounds.  Some of them have to be applied before
    calling ohci_start() some after, which necessitates a fair amount of
    code motion.  The other platform drivers require much smaller changes.
    
    The complete sb800_prefetch() function moved to ohci-q.c,because its
    only related to ohci-pci driver.
    
    USB_OHCI_HCD_PCI symbol no longer dependence on STB03xxx, PPC_MPC52xx and
    USB_OHCI_HCD_PPC_OF that's what removed.
    
    V2:
      - few specific content of pci related code in ohci_pci_start function has been moved to ohci_pci_reset
        and rest of the generic code is written in ohci_start of ohci-hcd.c file.
    V3:
     - ohci_restart() has been called in ohci_pci_reset() function for to reset the ohci pci.
    
    V4:
     -sb800_prefetch() moved to ohci-q.c,because its only related to ohci-pci.
     -no longer _creating_ CONFIG_USB_OHCI_PCI,creating CONFIG_USB_OHCI_HCD_PCI.
     -overrides renamed with pci_override,its giving proper meaning.
    
    V5:
     -sb800_prefetch() moved to pci-quirks.c,because its only related to pci.
    
    V6:
     -sb800_prefetch() function has been moved to pci-quirks.c made as separate patch in 2/3.
     -Most of the generic ohci pci changes moved in 2/3 patch,now this is complete  ohci-pci separation patch.
    
    V7:
     -Unrelated include file has been removed from ohci.h file.
    
    V8:
     -USB_OHCI_HCD_PCI symbol does not dependence on STB03xxx, PPC_MPC52xx and USB_OHCI_HCD_PPC_OF.
    
    Signed-off-by: Manjunath Goudar <manjunath.goudar@linaro.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ohci-hcd.c
+++ drivers/usb/host/ohci-hcd.c
@@ -1174,11 +1174,6 @@ MODULE_AUTHOR (DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE ("GPL");
 
-#ifdef CONFIG_PCI
-#include "ohci-pci.c"
-#define PCI_DRIVER		ohci_pci_driver
-#endif
-
 #if defined(CONFIG_ARCH_SA1100) && defined(CONFIG_SA1111)
 #include "ohci-sa1111.c"
 #define SA1111_DRIVER		ohci_hcd_sa1111_driver
@@ -1274,7 +1269,7 @@ MODULE_LICENSE ("GPL");
 #define PLATFORM_DRIVER		ohci_platform_driver
 #endif
 
-#if	!defined(PCI_DRIVER) &&		\
+#if	!IS_ENABLED(CONFIG_USB_OHCI_HCD_PCI) &&	\
 	!defined(PLATFORM_DRIVER) &&	\
 	!defined(OMAP1_PLATFORM_DRIVER) &&	\
 	!defined(OMAP3_PLATFORM_DRIVER) &&	\
@@ -1347,12 +1342,6 @@ static int __init ohci_hcd_mod_init(void)
 	retval = sa1111_driver_register(&SA1111_DRIVER);
 	if (retval < 0)
 		goto error_sa1111;
-#endif
-
-#ifdef PCI_DRIVER
-	retval = pci_register_driver(&PCI_DRIVER);
-	if (retval < 0)
-		goto error_pci;
 #endif
 
 #ifdef SM501_OHCI_DRIVER
@@ -1448,10 +1437,6 @@ static int __init ohci_hcd_mod_init(void)
 	platform_driver_unregister(&SM501_OHCI_DRIVER);
  error_sm501:
 #endif
-#ifdef PCI_DRIVER
-	pci_unregister_driver(&PCI_DRIVER);
- error_pci:
-#endif
 #ifdef SA1111_DRIVER
 	sa1111_driver_unregister(&SA1111_DRIVER);
  error_sa1111:
@@ -1515,9 +1500,6 @@ static void __exit ohci_hcd_mod_exit(void)
 #endif
 #ifdef SM501_OHCI_DRIVER
 	platform_driver_unregister(&SM501_OHCI_DRIVER);
-#endif
-#ifdef PCI_DRIVER
-	pci_unregister_driver(&PCI_DRIVER);
 #endif
 #ifdef SA1111_DRIVER
 	sa1111_driver_unregister(&SA1111_DRIVER);
