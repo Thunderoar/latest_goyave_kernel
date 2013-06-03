0229c39bfa102fe54e478f2242fa3655933c20c4
Ville Syrjälä <ville.syrjala@linux.intel.com>
2013-06-03 16:11:41 +0300

    drm/i915: Drop bogus fbdev sprite disable code
    plane->enabled is never set, so this code didn't do anything.
    
    Also drm_fb_helper_restore_fbdev_mode() will now disable all cursors
    and sprites for us, so we don't have to bother anymore.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@gmail.com>

--- drivers/gpu/drm/i915/intel_fb.c
+++ drivers/gpu/drm/i915/intel_fb.c
@@ -292,9 +292,7 @@ void intel_fb_output_poll_changed(struct drm_device *dev)
 void intel_fb_restore_mode(struct drm_device *dev)
 {
 	int ret;
-	struct drm_i915_private *dev_priv = dev->dev_private;
-	struct drm_mode_config *config = &dev->mode_config;
-	struct drm_plane *plane;
+	drm_i915_private_t *dev_priv = dev->dev_private;
 
 	if (INTEL_INFO(dev)->num_pipes == 0)
 		return;
@@ -304,11 +302,6 @@ void intel_fb_restore_mode(struct drm_device *dev)
 	ret = drm_fb_helper_restore_fbdev_mode(&dev_priv->fbdev->helper);
 	if (ret)
 		DRM_DEBUG("failed to restore crtc mode\n");
-
-	/* Be sure to shut off any planes that may be active */
-	list_for_each_entry(plane, &config->plane_list, head)
-		if (plane->enabled)
-			plane->funcs->disable_plane(plane);
 
 	drm_modeset_unlock_all(dev);
 }
