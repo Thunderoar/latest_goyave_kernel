c0ce44f5468cb0705c9118f52cb8a06fa08035b6
Borislav Petkov <bp@suse.de>
2013-06-09 12:07:33 +0200

    x86: Add a static_cpu_has_safe variant
    We want to use this in early code where alternatives might not have run
    yet and for that case we fall back to the dynamic boot_cpu_has.
    
    For that, force a 5-byte jump since the compiler could be generating
    differently sized jumps for each label.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: http://lkml.kernel.org/r/1370772454-6106-5-git-send-email-bp@alien8.de
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1378,3 +1378,9 @@ void warn_pre_alternatives(void)
 }
 EXPORT_SYMBOL_GPL(warn_pre_alternatives);
 #endif
+
+inline bool __static_cpu_has_safe(u16 bit)
+{
+	return boot_cpu_has(bit);
+}
+EXPORT_SYMBOL_GPL(__static_cpu_has_safe);
