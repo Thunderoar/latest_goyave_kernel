077d80be72dcabb6c2bf47348372b6c8c155752e
Daniel Stone <daniel@fooishbar.org>
2013-10-31 00:25:34 -0700

    Input: evdev - fall back to vmalloc for client event buffer
    commit 92eb77d0ffbaa71b501a0a8dabf09a351bf4267f upstream.
    
    evdev always tries to allocate the event buffer for clients using
    kzalloc rather than vmalloc, presumably to avoid mapping overhead where
    possible.  However, drivers like bcm5974, which claims support for
    reporting 16 fingers simultaneously, can have an extraordinarily large
    buffer.  The resultant contiguous order-4 allocation attempt fails due
    to fragmentation, and the device is thus unusable until reboot.
    
    Try kzalloc if we can to avoid the mapping overhead, but if that fails,
    fall back to vzalloc.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -299,8 +299,6 @@ static int evdev_release(struct inode *inode, struct file *file)
 	mutex_unlock(&evdev->mutex);
 
 	evdev_detach_client(evdev, client);
-	if (client->use_wake_lock)
-		wake_lock_destroy(&client->wake_lock);
 
 	if (is_vmalloc_addr(client))
 		vfree(client);
@@ -332,7 +330,7 @@ static int evdev_open(struct inode *inode, struct file *file)
 
 	client = kzalloc(size, GFP_KERNEL | __GFP_NOWARN);
 	if (!client)
-		client = vzalloc(size); 
+		client = vzalloc(size);
 	if (!client)
 		return -ENOMEM;
 
