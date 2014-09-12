e6c1573cc9056b90549406844d35bc582bf94d91
Rob Herring <robherring2@gmail.com>
2014-09-12 11:32:24 -0700

    pstore-ram: Fix hangs by using write-combine mappings

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -345,13 +345,7 @@ static void *persistent_ram_vmap(phys_addr_t start, size_t size)
 	page_start = start - offset_in_page(start);
 	page_count = DIV_ROUND_UP(size + offset_in_page(start), PAGE_SIZE);
 
-	/* ldrex of atomic_cmpxchg called by buffer_size_add and buffer_start_add
-	 * need cache */
-#if 0
-	prot = pgprot_noncached(PAGE_KERNEL);
-#else
-	prot = PAGE_KERNEL;
-#endif
+	prot = pgprot_writecombine(PAGE_KERNEL);
 
 	pages = kmalloc(sizeof(struct page *) * page_count, GFP_KERNEL);
 	if (!pages) {
@@ -378,7 +372,7 @@ static void *persistent_ram_iomap(phys_addr_t start, size_t size)
 		return NULL;
 	}
 
-	return ioremap(start, size);
+	return ioremap_wc(start, size);
 }
 
 static int persistent_ram_buffer_map(phys_addr_t start, phys_addr_t size,
