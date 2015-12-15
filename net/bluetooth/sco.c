b9d1d80529b0c453d93ed2e814e06bf0852fe6eb
David S. Miller <davem@davemloft.net>
2015-12-15 15:39:08 -0500

    bluetooth: Validate socket address length in sco_sock_bind().
    [ Upstream commit 5233252fce714053f0151680933571a2da9cbfb4 ]
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/bluetooth/sco.c
+++ net/bluetooth/sco.c
@@ -448,7 +448,8 @@ static int sco_sock_create(struct net *net, struct socket *sock, int protocol,
 	return 0;
 }
 
-static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
+static int sco_sock_bind(struct socket *sock, struct sockaddr *addr,
+			 int addr_len)
 {
 	struct sockaddr_sco sa;
 	struct sock *sk = sock->sk;
@@ -459,9 +460,8 @@ static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
 	if (!addr || addr->sa_family != AF_BLUETOOTH)
 		return -EINVAL;
 
-	memset(&sa, 0, sizeof(sa));
-	len = min_t(unsigned int, sizeof(sa), alen);
-	memcpy(&sa, addr, len);
+	if (addr_len < sizeof(struct sockaddr_sco))
+		return -EINVAL;
 
 	lock_sock(sk);
 
