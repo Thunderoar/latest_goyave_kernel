14d16e286f43f1d3e8ce41605318c578c7f1f3e0
Thunderoar <farhansamilme@gmail.com>
2017-06-08 03:45:14 +0800

    mm: close PageTail race

--- mm/swap.c
+++ mm/swap.c
@@ -99,7 +99,7 @@ static void put_compound_page(struct page *page)
 
 	if (unlikely(PageTail(page))) {
 		/* __split_huge_page_refcount can run under us */
-		struct page *page_head = compound_trans_head(page);
+		struct page *page_head = compound_head(page);
 
 		if (likely(page != page_head &&
 			   get_page_unless_zero(page_head))) {
@@ -214,7 +214,7 @@ bool __get_page_tail(struct page *page)
 	} else {
 		unsigned long flags;
 
-		page_head = compound_trans_head(page);
+	struct page *page_head = compound_head(page);
 		if (likely(page != page_head &&
 					get_page_unless_zero(page_head))) {
 
