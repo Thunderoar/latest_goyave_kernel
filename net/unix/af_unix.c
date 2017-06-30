806302b5b3289a05bbd9ec1ea60d89d7fc4c7880
Thunderoar <farhansamilme@gmail.com>
2017-06-30 17:02:19 +0800

    net: rework recvmsg handler msg_name and msg_namelen logic

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1778,7 +1778,6 @@ static void unix_copy_addr(struct msghdr *msg, struct sock *sk)
 {
 	struct unix_sock *u = unix_sk(sk);
 
-	msg->msg_namelen = 0;
 	if (u->addr) {
 		msg->msg_namelen = u->addr->len;
 		memcpy(msg->msg_name, u->addr->name, u->addr->len);
@@ -1942,8 +1941,6 @@ static int unix_stream_recvmsg(struct kiocb *iocb, struct socket *sock,
 
 	target = sock_rcvlowat(sk, flags&MSG_WAITALL, size);
 	timeo = sock_rcvtimeo(sk, flags&MSG_DONTWAIT);
-
-	msg->msg_namelen = 0;
 
 	/* Lock the socket to prevent queue disordering
 	 * while sleeps in memcpy_tomsg
