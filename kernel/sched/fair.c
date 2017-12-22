1a0dc4032b5a3fb49a341265d39761f25d492cbf
Thunderoar <farhansamilme@gmail.com>
2017-12-23 04:17:20 +0800

    Implemented Fast-IDLING of CPU

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4781,7 +4781,7 @@ static inline void update_sd_lb_stats(struct lb_env *env,
 
 		local_group = cpumask_test_cpu(env->dst_cpu, sched_group_cpus(sg));
 		memset(&sgs, 0, sizeof(sgs));
-		update_sg_lb_stats(env, sg, load_idx, local_group, balance, &sgs);
+		update_sg_lb_stats(env, sg, load_idx, local_group, balance, &sgs, &overload);
 
 		if (local_group && !(*balance))
 			return;
@@ -4823,6 +4823,13 @@ static inline void update_sd_lb_stats(struct lb_env *env,
 
 		sg = sg->next;
 	} while (sg != env->sd->groups);
+
+	if (!env->sd->parent) {
+	 /* update overload indicator if we are at root domain */
+		if (env->dst_rq->rd->overload != overload)
+			env->dst_rq->rd->overload = overload;
+	}
+
 }
 
 /**
@@ -5453,7 +5460,8 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 
 	this_rq->idle_stamp = this_rq->clock;
 
-	if (this_rq->avg_idle < sysctl_sched_migration_cost)
+	if (this_rq->avg_idle < sysctl_sched_migration_cost ||
+					!this_rq->rd->overload)
 		return;
 
 	/*
