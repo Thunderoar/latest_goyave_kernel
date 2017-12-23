e823a94d6cfd6bacf7d58e72c82abdf9b7a97623
Thunderoar <farhansamilme@gmail.com>
2017-12-23 12:24:41 +0800

    finalizing camera fix

--- include/video/sensor_drv_k.h
+++ include/video/sensor_drv_k.h
@@ -90,6 +90,7 @@ typedef struct sensor_power_info {
 	uint32_t            op_sensor_id;
 	struct sensor_power main_sensor;
 	struct sensor_power sub_sensor;
+	uint8_t i2c_addr;
 } SENSOR_POWER_CFG_T;
 
 
