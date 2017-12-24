e42db5c0010139117cf2340353e31289bf7aa521
Thunderoar <farhansamilme@gmail.com>
2017-12-24 08:07:24 +0800

    sched: change find_busiest_queue to h_nr_running

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5166,7 +5166,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 * When comparing with imbalance, use weighted_cpuload()
 		 * which is not scaled with the cpu power.
 		 */
-		if (capacity && rq->nr_running == 1 && wl > env->imbalance)
+		if (capacity && rq->cfs.h_nr_running == 1 && wl > imbalance)
 			continue;
 
 		/*
