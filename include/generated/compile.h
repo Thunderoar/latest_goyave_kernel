744e0affcbd6a4c786ef1c5fb29e0786045fa23b
Thunderoar <farhansamilme@gmail.com>
2018-03-03 20:00:26 +0800

    ARM: convert all 'mov.* pc, reg' to 'bx reg' for ARMv6+

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#15 SMP PREEMPT Sat Mar 3 19:42:59 +08 2018"
-=======
-/* This file is auto generated, version 11 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Sat Mar 3 08:54:21 +08 2018"
->>>>>>> parent of b397c16... block: Add support for reinsert a dispatched req
+#define UTS_VERSION "#16 SMP PREEMPT Sat Mar 3 19:54:07 +08 2018"
 #define LINUX_COMPILE_BY "thn-dev"
 #define LINUX_COMPILE_HOST "thn-dev"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
