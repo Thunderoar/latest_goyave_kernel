d17a039b16315661cd48accb5bfd2c2449e03e57
Thunderoar <farhansamilme@gmail.com>
2018-03-03 19:10:19 +0800

    Optimized Integer SQRT. for upto 3x faster operation

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Sat Mar 3 18:52:41 +08 2018"
+#define UTS_VERSION "#13 SMP PREEMPT Sat Mar 3 19:02:17 +08 2018"
 #define LINUX_COMPILE_BY "thn-dev"
 #define LINUX_COMPILE_HOST "thn-dev"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
