780ea02953b5ebf2d9ecd0d8463c842ea3034daa
Thunderoar <farhansamilme@gmail.com>
2018-03-03 20:37:43 +0800

    Revert "dm: Request based dm-crypt"
    This reverts commit 362c414f206f701258d1701d226bd140b299e843.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 SMP PREEMPT Sat Mar 3 20:01:20 +08 2018"
+#define UTS_VERSION "#16 SMP PREEMPT Sat Mar 3 19:54:07 +08 2018"
 #define LINUX_COMPILE_BY "thn-dev"
 #define LINUX_COMPILE_HOST "thn-dev"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
