6e77a8b37d2a0fdefb17f3f8b3e70dc00c87418c
Thunderoar <farhansamilme@gmail.com>
2018-03-03 20:49:49 +0800

    decompressor: add LZ4 decompressor module

--- include/generated/bounds.h
+++ include/generated/bounds.h
@@ -7,7 +7,7 @@
  *
  */
 
-#define NR_PAGEFLAGS 22 /* __NR_PAGEFLAGS	@ */
+#define NR_PAGEFLAGS 23 /* __NR_PAGEFLAGS	@ */
 #define MAX_NR_ZONES 3 /* __MAX_NR_ZONES	@ */
 #define NR_PCG_FLAGS 3 /* __NR_PCG_FLAGS	@ */
 
