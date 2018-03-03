3dcff42145b942279ce6ed6160819d4d7a3d4c47
Thunderoar <farhansamilme@gmail.com>
2018-03-03 19:51:00 +0800

    revert block

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 /* This file is auto generated, version 15 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
 #define UTS_VERSION "#15 SMP PREEMPT Sat Mar 3 19:42:59 +08 2018"
+=======
+/* This file is auto generated, version 11 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 SMP PREEMPT Sat Mar 3 08:54:21 +08 2018"
+>>>>>>> parent of b397c16... block: Add support for reinsert a dispatched req
 #define LINUX_COMPILE_BY "thn-dev"
 #define LINUX_COMPILE_HOST "thn-dev"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
