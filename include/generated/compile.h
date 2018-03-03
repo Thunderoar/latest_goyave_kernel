8d1d4168b5821176b28eead3afea43f89cac3e43
Thunderoar <farhansamilme@gmail.com>
2018-03-03 19:46:18 +0800

    '

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 15 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Sat Mar 3 19:16:29 +08 2018"
+#define UTS_VERSION "#15 SMP PREEMPT Sat Mar 3 19:42:59 +08 2018"
 #define LINUX_COMPILE_BY "thn-dev"
 #define LINUX_COMPILE_HOST "thn-dev"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
