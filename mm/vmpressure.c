6b1b23c8fad2bf24b6fb1a075c8927cfae7b1d83
Thunderoar <farhansamilme@gmail.com>
2018-03-20 06:25:45 +0800

    mm: vmpressure: account allocstalls only on higher pressures

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -182,8 +182,12 @@ out:
 static unsigned long vmpressure_account_stall(unsigned long pressure,
 				unsigned long stall, unsigned long scanned)
 {
-	unsigned long scale =
-		((vmpressure_scale_max - pressure) * stall) / scanned;
+	unsigned long scale;
+
+	if (pressure < 70)
+		return pressure;
+
+	scale = ((vmpressure_scale_max - pressure) * stall) / scanned;
 
 	return pressure + scale;
 }
