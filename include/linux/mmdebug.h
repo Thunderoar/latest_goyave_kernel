43b21c4c0c4cdb98790bd1730cf87665b8624a86
Thunderoar <farhansamilme@gmail.com>
2018-03-25 19:06:20 +0800

    Revert "mm: introduce dump_vma"
    This reverts commit 06463fd8b5963d6f8a0702f23564b3953caf6009.

--- include/linux/mmdebug.h
+++ include/linux/mmdebug.h
@@ -1,16 +1,6 @@
 #ifndef LINUX_MM_DEBUG_H
 #define LINUX_MM_DEBUG_H 1
 
-#include <linux/stringify.h>
-
-struct page;
-struct vm_area_struct;
-
-extern void dump_page(struct page *page, const char *reason);
-extern void dump_page_badflags(struct page *page, const char *reason,
-			       unsigned long badflags);
-void dump_vma(const struct vm_area_struct *vma);
-
 #ifdef CONFIG_DEBUG_VM
 #define VM_BUG_ON(cond) BUG_ON(cond)
 #else
