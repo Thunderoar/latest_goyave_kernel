325d1a3893ef3e30fdaa55976c1f06a219bb9c8d
Thunderoar <farhansamilme@gmail.com>
2018-04-10 05:23:47 +0800

    Revert "mmc: block: Enable runtime pm for mmc blkdevice"
    This reverts commit d51bb4a8c1b44a60ef635dbd5dd6ecec07686733.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1491,14 +1491,14 @@ static void mmc_detect(struct mmc_host *host)
 	BUG_ON(!host);
 	BUG_ON(!host->card);
 
-	mmc_get_card(host->card);
+	mmc_claim_host(host);
 
 	/*
 	 * Just check if our card has been removed.
 	 */
 	err = _mmc_detect_card_removed(host);
 
-	mmc_put_card(host->card);
+	mmc_release_host(host);
 
 	if (err) {
 		mmc_remove(host);
