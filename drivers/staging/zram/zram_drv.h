e324b4f7201f8ac93cf9358eae0ee866d0b964dd
Thunderoar <farhansamilme@gmail.com>
2018-04-14 16:38:30 +0800

    zram: finalizing without hurting its function

--- drivers/staging/zram/zram_drv.h
+++ drivers/staging/zram/zram_drv.h
@@ -112,6 +112,8 @@ struct zram {
 	struct zram_stats stats;
 };
 
+static struct zram *zram_devices;
+unsigned int zram_get_num_devices(void);
 #ifdef CONFIG_SYSFS
 extern struct attribute_group zram_disk_attr_group;
 #endif
