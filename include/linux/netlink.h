ae22a5547b647de2ada7fcc3c19d844af2bc8b81
Thunderoar <farhansamilme@gmail.com>
2018-04-22 21:29:46 +0800

    Revert "net: netlink: virtual tap device management"
    This reverts commit 5a8a41d51158a9cba7139b50630e2b59a15c2bee.

--- include/linux/netlink.h
+++ include/linux/netlink.h
@@ -162,14 +162,11 @@ static inline int netlink_dump_start(struct sock *ssk, struct sk_buff *skb,
 	return __netlink_dump_start(ssk, skb, nlh, control);
 }
 
-struct netlink_tap {
-	struct net_device *dev;
-	struct module *module;
-	struct list_head list;
-};
-
-extern int netlink_add_tap(struct netlink_tap *nt);
-extern int __netlink_remove_tap(struct netlink_tap *nt);
-extern int netlink_remove_tap(struct netlink_tap *nt);
+bool __netlink_ns_capable(const struct netlink_skb_parms *nsp,
+			  struct user_namespace *ns, int cap);
+bool netlink_ns_capable(const struct sk_buff *skb,
+			struct user_namespace *ns, int cap);
+bool netlink_capable(const struct sk_buff *skb, int cap);
+bool netlink_net_capable(const struct sk_buff *skb, int cap);
 
 #endif	/* __LINUX_NETLINK_H */
