d483d201f550c719f553f4c4ddee170f8d2d6f3e
Thunderoar <farhansamilme@gmail.com>
2018-05-12 04:08:12 +0800

    kernel/power/main: fix compile

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -372,7 +372,7 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 	for (s = &pm_states[state]; state < PM_SUSPEND_MAX; s++, state++) {
 		if (*s && len == strlen(*s) && !strncmp(buf, *s, len)) {
 #ifdef CONFIG_EARLYSUSPEND
-			if (state == PM_SUSPEND_ON || valid_state(state)) {
+			if (state == PM_SUSPEND_ON || pm_states[state].state) {
 				error = 0;
 				request_suspend_state(state);
 				break;
