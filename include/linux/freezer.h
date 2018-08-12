296f1ef5496c7a4b90ea4ae5391884062c5192fa
Thunderoar <farhansamilme@gmail.com>
2018-08-12 22:10:34 +0800

    cleanup

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -206,14 +206,6 @@ static inline long freezable_schedule_timeout_interruptible(long timeout)
 	return __retval;
 }
 
-/* DO NOT ADD ANY NEW CALLERS OF THIS FUNCTION */
-static inline void freezable_schedule_unsafe(void)
-{
-	freezer_do_not_count();
-	schedule();
-	freezer_count_unsafe();
-}
-
 /* Like schedule_timeout_killable(), but should not block the freezer. */
 static inline long freezable_schedule_timeout_killable(long timeout)
 {
@@ -245,16 +237,6 @@ static inline int freezable_schedule_hrtimeout_range(ktime_t *expires,
 	freezer_do_not_count();
 	__retval = schedule_hrtimeout_range(expires, delta, mode);
 	freezer_count();
-	return __retval;
-}
-
-/* DO NOT ADD ANY NEW CALLERS OF THIS FUNCTION */
-static inline long freezable_schedule_timeout_killable_unsafe(long timeout)
-{
-	long __retval;
-	freezer_do_not_count();
-	__retval = schedule_timeout_killable(timeout);
-	freezer_count_unsafe();
 	return __retval;
 }
 
