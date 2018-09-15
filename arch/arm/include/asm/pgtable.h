ca1ca19093b0ac286e6cb470b10e62b25087bc0a
Thunderoar <farhansamilme@gmail.com>
2018-09-16 00:29:34 +0800

    arm: Add XN bit to pgrprot_stronglyordered
    pgprot_stronglyordered sets page attributes to the strongly
    ordered memory type. This type is often used for regions of memory which
    cannot be speculated into. Strongly ordered memory by itself prevents
    speculative data accesses but it will not prevent speculative instruction
    accesses. Since strongly ordered memory will never be used for
    instructions, set the XN bit as well to prevent speculative instruction
    fethces.
    
    Bug: 10750953
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Mekala Natarajan <mekalan@codeaurora.org>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -120,7 +120,7 @@ extern pgprot_t		pgprot_s2_device;
 	__pgprot_modify(prot, L_PTE_MT_MASK, L_PTE_MT_DEV_CACHED)
 
 #define pgprot_stronglyordered(prot) \
-	__pgprot_modify(prot, L_PTE_MT_MASK, L_PTE_MT_UNCACHED)
+	__pgprot_modify(prot, L_PTE_MT_MASK, L_PTE_MT_UNCACHED | L_PTE_XN)
 
 #ifdef CONFIG_ARM_DMA_MEM_BUFFERABLE
 #define pgprot_dmacoherent(prot) \
