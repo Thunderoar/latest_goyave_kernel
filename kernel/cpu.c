12437e623e99e22832fa5487bbf3abf999ca061c
Thunderoar <farhansamilme@gmail.com>
2021-08-05 14:35:58 +0800

    kernel/cpu: fix compile

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -195,6 +195,7 @@ static int cpu_notify(unsigned long val, void *v)
 	return __cpu_notify(val, v, -1, NULL);
 }
 
+#ifdef CONFIG_HOTPLUG_CPU
 static void cpu_notify_nofail(unsigned long val, void *v)
 {
 	BUG_ON(cpu_notify(val, v));
